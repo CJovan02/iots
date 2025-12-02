c1fe0af3cbd617d1a034c612920b0e7bbd9ba6d3
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2025-12-02 15:31:43 +0100

    created dataset domain model

---
+++ Project1-Microservices/DataManager/internal/domain/sensor_reading.go
@@ -0,0 +1,16 @@
+package domain
+
+import "time"
+
+// SensorReading represents necessary columns from the dataset for fire detection
+type SensorReading struct {
+	Timestamp   time.Time // UTC timestamp
+	Temperature float64   // Air temperature, fires raise temperature
+	Humidity    float64   // Air humidity, very high or low can indicate fire
+	TVOC        uint16    // Total Volatile Organic Compounds, high numbers indicate fire
+	ECO2        uint16    // CO2 equivalent concentration, indirect signal for combustion
+	RawHw       uint16    // Raw molecular hydrogen, additional chemical signal
+	RawEthanol  uint16    // Raw ethanol gas, additional chemical signal
+	PM25        float64   // Particulate matter <2.5 µm, smoke increases concentrations
+	FireAlarm   uint8     // Ground truth, 1 if fire is present
+}
