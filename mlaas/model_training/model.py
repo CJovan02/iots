0cf8a1242433108a516fffff4904b5ff3b674ba7
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2025-12-27 01:24:16 +0100

    Changed model training to neural network

--- mlaas/model_training/model.py
+++ mlaas/model_training/model.py
@@ -1,45 +1,76 @@
-import pandas as pd
-from pandas import DataFrame, Series
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.python.ops.confusion_matrix import confusion_matrix
 
-def load_dataset():
-    df = pd.read_csv("../data/smoke_detection_iot.csv")
+# We don't use all the features since the server doesn't store all the features
+# This is for project simplicity reasons
+features = ["Temperature[C]", "Humidity[%]", "TVOC[ppb]", "eCO2[ppm]", "Raw H2", "Raw Ethanol", "PM2.5", ]
+label = "Fire Alarm"
+1
+df = pd.read_csv("../data/smoke_detection_iot.csv")
 
-    features = df.drop(
-        columns=["Fire Alarm", "UTC", "Unnamed: 0", "CNT"]
-    )
-    labels = df["Fire Alarm"]
+x = df[features]
+y = df[label]
 
-    x = features.values
-    y = labels.values
-    window_size = 50
-    future_window = 200
+# y.plot()
+# plt.show()
 
-    x_new, y_new = [], []
-    for i in range(len(x) - window_size - future_window):
-        x_window = x[i: i + window_size].flatten()
-        y_window = y[i + window_size: i + window_size + future_window].max()
-        x_new.append(x_window)
-        y_new.append(y_window)
+print(x.head())
 
-    return np.array(x_new), np.array(y_new)
+scaler = StandardScaler()
+x = scaler.fit_transform(x)
+
+x_df = pd.DataFrame(x)
+print(x_df.head())
+print(x_df.describe())
 
 
-x, y = load_dataset()
+def create_sliding_window(x, y, window_size) -> tuple[np.ndarray, np.ndarray]:
+    x_win, y_win = [], []
+    for i in range(len(x) - window_size):
+        x_win.append(x[i: i + window_size])
+        y_win.append(y[i + window_size])
 
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y,
-    test_size=0.2,
-    shuffle=False
-)
+    return np.array(x_win), np.array(y_win)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(x_train, y_train)
 
+window_size = 30
+x_win, y_win = create_sliding_window(x, y, window_size)
+print(x_win.shape, y_win.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x_win, y_win, test_size=0.2, shuffle=False)
+# x_train, y_train = x[:51142], y[:51142]
+# x_test, y_test = x[51142:], y[51142:]
+print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+# Neural network
+model = Sequential()
+model.add(LSTM(
+    32,
+    input_shape=(window_size, x_win.shape[2])
+))
+model.add(Dense(1, activation='sigmoid'))
+
+print(model.summary())
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Training
+model.fit(x_train, y_train, epochs=10, batch_size=64, validation_split=0.2)
+
+# Evaluation
+loss, acc = model.evaluate(x_test, y_test)
+print(f"Test Accuracy: {acc:.4f}")
+
+# Prediction
 y_pred = model.predict(x_test)
+y_pred = (y_pred > 0.5).astype(int)
 
 print(classification_report(y_test, y_pred))
 print(confusion_matrix(y_test, y_pred))
