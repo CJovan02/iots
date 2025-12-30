5d2c6d02f0f2c271d6b3d9ba7a267e752a8f297f
Jovan Cvetkovic <cvetkovicjovanwork@gmail.com>
2025-12-30 21:27:47 +0100

    Created mlaas docker image and compose

--- mlaas/app/main.py
+++ mlaas/app/main.py
@@ -1,10 +1,15 @@
+from pathlib import Path
+
 from fastapi import FastAPI
 from tensorflow.keras.models import load_model
 
 from app.dto import PredictionRequest
 from app.utils.converters import y_to_float, request_to_x
 
-model = load_model("../model_artifacts/model.keras")
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_PATH = BASE_DIR / "model_artifacts" / "model.keras"
+
+model = load_model(MODEL_PATH)
 app = FastAPI()
 
 @app.get("/")
